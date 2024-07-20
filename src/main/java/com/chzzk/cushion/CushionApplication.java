5bd67560aa3d0d61aa157540955138ca2edaadfb
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-20 14:40:28 +0900

    init project

---
+++ src/main/java/com/chzzk/cushion/CushionApplication.java
@@ -0,0 +1,13 @@
+package com.chzzk.cushion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CushionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CushionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chzzk/cushion/CushionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chzzk.cushion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CushionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
