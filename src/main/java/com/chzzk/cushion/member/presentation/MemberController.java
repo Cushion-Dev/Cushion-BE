a327f541084d3141aeb3a5b1b11fd813ae711766
gywns0417 <gywns0417@gmail.com>
2024-07-24 02:15:30 +0900

    feat: 소셜 로그인 로그아웃 기능 구현

--- src/main/java/com/chzzk/cushion/member/presentation/MemberController.java
+++ src/main/java/com/chzzk/cushion/member/presentation/MemberController.java
@@ -1,10 +1,15 @@
 package com.chzzk.cushion.member.presentation;
 
+import com.chzzk.cushion.member.application.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -15,6 +20,8 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequestMapping("/members")
 @Tag(name = "member", description = "회원 API")
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/login/oauth2/naver")
     @Operation(summary = "소셜 로그인_네이버", description = "네이버 소셜 로그인을 위한 URL을 리다이렉트 합니다.")
@@ -32,5 +39,11 @@ public class MemberController {
     @Operation(summary = "소셜 로그인_구글", description = "구글 소셜 로그인을 위한 URL을 리다이렉트 합니다.")
     public RedirectView loginToGoogle() {
         return new RedirectView("/oauth2/authorization/google");
+    }
+
+    @PostMapping("/logout")
+    @Operation(summary = "로그아웃", description = "소셜 로그인한 계정을 로그아웃합니다.")
+    public RedirectView logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        return memberService.logout(request, response, authentication);
     }
 }
