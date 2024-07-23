8bc540fa5348397d10292766ccc6d65cfa1f190d
gywns0417 <gywns0417@gmail.com>
2024-07-23 21:09:49 +0900

    feat: 공통 예외 처리 클래스 작성

---
+++ src/main/java/com/chzzk/cushion/global/exception/ErrorCode.java
@@ -0,0 +1,40 @@
+package com.chzzk.cushion.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // 400
+    FILE_SIZE_EXCEEDED(BAD_REQUEST, "파일 크기가 초과되었습니다."),
+    EMPTY_JWT_TOKEN(BAD_REQUEST, "토큰 값이 존재하지 않습니다."),
+
+    // 401
+    MISMATCHED_EMAIL_OR_PASSWORD(UNAUTHORIZED, "이메일 또는 비밀번호가 잘못되었습니다."),
+    AUTHENTICATION_ERROR(UNAUTHORIZED, "인증 오류가 발생했습니다."),
+    BAD_CREDENTIAL_ERROR(UNAUTHORIZED, "로그인에 실패했습니다."),
+    AUTHENTICATION_REQUIRED(UNAUTHORIZED, "인증이 필요합니다."),
+    EXPIRED_JWT_TOKEN(UNAUTHORIZED, "만료된 토큰 입니다."),
+    INVALID_JWT_TOKEN(UNAUTHORIZED, "유효하지 않은 토큰입니다"),
+    UNAUTHORIZED_REISSUE_TOKEN(UNAUTHORIZED, "로그아웃 상태에서 토큰을 재발급 받을 수 없습니다."),
+
+    // 403
+
+    // 404
+    NOT_FOUND_MEMBER(NOT_FOUND, "존재하지 않는 회원입니다."),
+
+
+    // 409
+    ALREADY_REGISTERED_EMAIL(CONFLICT, "이미 등록된 이메일입니다.");
+
+    // 500
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
