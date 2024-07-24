1bda856112250d249d253bce729e5175cfbf071e
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-25 00:04:05 +0900

    feat: Message, ChatRoom 엔티티 클래스 작성 및 연관관계 매핑

---
+++ src/main/java/com/chzzk/cushion/chatroom/domain/Message.java
@@ -0,0 +1,27 @@
+package com.chzzk.cushion.chatroom.domain;
+
+import com.chzzk.cushion.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Message extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    @NotBlank
+    @Size(max = 400)
+    private String content;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private SenderType senderType;
+}
--- src/main/java/com/chzzk/cushion/member/domain/Member.java
+++ src/main/java/com/chzzk/cushion/member/domain/Member.java
@@ -1,11 +1,15 @@
 package com.chzzk.cushion.member.domain;
 
+import com.chzzk.cushion.chatroom.domain.ChatRoom;
 import com.chzzk.cushion.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -28,4 +32,7 @@ public class Member extends BaseTimeEntity {
     private String password;
 
     private String username;
+
+    @OneToMany(mappedBy = "member")
+    private List<ChatRoom> chatRooms = new ArrayList<>();
 }
