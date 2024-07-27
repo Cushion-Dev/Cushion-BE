509b6e50bc8341fa6b2383b5c542ea8583b84cb0
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-07-27 22:53:01 +0900

    feat: 사용자 정보 추가 설정 API 검증 메시지 추가

--- src/main/java/com/chzzk/cushion/member/dto/MemberDto.java
+++ src/main/java/com/chzzk/cushion/member/dto/MemberDto.java
@@ -12,17 +12,17 @@ import lombok.Data;
 public class MemberDto {
 
     @Schema(description = "소속", example = "쿠션 회사")
-    @NotBlank
-    @Size(max = 15)
+    @NotBlank(message = "소속은 공백을 허용하지 않습니다.")
+    @Size(max = 15, message = "소속 글자수는 0에서 15 사이여야 합니다.")
     private String affiliation;
 
     @Schema(description = "직무", example = "개발")
-    @NotBlank
-    @Size(max = 15)
+    @NotBlank(message = "직무는 공백을 허용하지 않습니다.")
+    @Size(max = 15, message = "직무 글자수는 0에서 15 사이여야 합니다.")
     private String job;
 
     @Schema(description = "이름", example = "홍길동")
-    @NotBlank
-    @Size(max = 15)
+    @NotBlank(message = "이름은 공백을 허용하지 않습니다.")
+    @Size(max = 15, message = "이름 글자수는 0에서 15 사이여야 합니다.")
     private String realName;
 }
