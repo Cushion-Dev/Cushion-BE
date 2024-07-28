82e30d0a1c5945054d1ff60face32d8b89612312
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-07-28 11:12:59 +0900

    hotfix: 쿠키 도메인 설정 제거

--- src/main/java/com/chzzk/cushion/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/chzzk/cushion/global/jwt/JwtTokenProvider.java
@@ -84,7 +84,6 @@ public class JwtTokenProvider {
         cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24); // accessToken 유효
-        cookie.setDomain("www.coocian.com");
         return cookie;
     }
 
@@ -95,7 +94,6 @@ public class JwtTokenProvider {
         cookie.setSecure(true); // TODO : HTTPS 적용 시 적용 가능
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
-        cookie.setDomain("www.coocian.com");
         return cookie;
     }
 
@@ -106,7 +104,6 @@ public class JwtTokenProvider {
         cookie.setSecure(true); // TODO : HTTPS 적용 시 적용 가능
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
-        cookie.setDomain("www.coocian.com");
         return cookie;
     }
 
