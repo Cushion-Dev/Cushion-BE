11e642201bcfedc6aac6d4b19e0e05b945cc54c4
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-07-28 12:53:33 +0900

    hotfix: 쿠키 path 설정

--- src/main/java/com/chzzk/cushion/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/chzzk/cushion/global/jwt/JwtTokenProvider.java
@@ -86,6 +86,7 @@ public class JwtTokenProvider {
         Cookie cookie = new Cookie(cookieName, refreshToken);
         cookie.setHttpOnly(false);
         cookie.setSecure(true);
+        cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24); // accessToken 유효
         return cookie;
     }
@@ -95,6 +96,7 @@ public class JwtTokenProvider {
         Cookie cookie = new Cookie(cookieName, accessToken);
         cookie.setHttpOnly(false);
         cookie.setSecure(true); // TODO : HTTPS 적용 시 적용 가능
+        cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
         return cookie;
     }
@@ -104,6 +106,7 @@ public class JwtTokenProvider {
         Cookie cookie = new Cookie(cookieName, memberId.toString());
         cookie.setHttpOnly(false);
         cookie.setSecure(true); // TODO : HTTPS 적용 시 적용 가능
+        cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
         return cookie;
     }
