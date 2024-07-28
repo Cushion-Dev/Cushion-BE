38dcc69e079546c2574d216b4248453ee4b7b56a
king_0417 <73704053+gywns0417@users.noreply.github.com>
2024-07-28 12:06:17 +0900

    hotfix: 쿠키 도메인재설정

--- src/main/java/com/chzzk/cushion/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/chzzk/cushion/global/jwt/JwtTokenProvider.java
@@ -83,7 +83,7 @@ public class JwtTokenProvider {
         Cookie cookie = new Cookie(cookieName, refreshToken);
         cookie.setHttpOnly(false);
         cookie.setSecure(true);
-        cookie.setPath("www.coocian.com");
+        cookie.setPath(".coocian.com");
         cookie.setDomain("/");
         cookie.setMaxAge(60 * 60 * 24); // accessToken 유효
         return cookie;
@@ -94,7 +94,7 @@ public class JwtTokenProvider {
         Cookie cookie = new Cookie(cookieName, accessToken);
         cookie.setHttpOnly(false);
         cookie.setSecure(true); // TODO : HTTPS 적용 시 적용 가능
-        cookie.setDomain("www.coocian.com");
+        cookie.setDomain(".coocian.com");
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
         return cookie;
@@ -105,7 +105,7 @@ public class JwtTokenProvider {
         Cookie cookie = new Cookie(cookieName, memberId.toString());
         cookie.setHttpOnly(false);
         cookie.setSecure(true); // TODO : HTTPS 적용 시 적용 가능
-        cookie.setDomain("www.coocian.com");
+        cookie.setDomain(".coocian.com");
         cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24);
         return cookie;
