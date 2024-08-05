8199f71d35aaaf403f9a6d19fa983de84f8fff9a
Hanjaemo <110653660+Hanjaemo@users.noreply.github.com>
2024-08-05 19:55:45 +0900

    feat: 새로고침 기능에서 이전 내용이 포함되어 변환되도록 개선

--- src/main/java/com/chzzk/cushion/style/application/RetryChangeStyleService.java
+++ src/main/java/com/chzzk/cushion/style/application/RetryChangeStyleService.java
@@ -5,10 +5,11 @@ import com.chzzk.cushion.chatroom.domain.Message;
 import com.chzzk.cushion.member.domain.Member;
 import com.chzzk.cushion.member.domain.MemberRepository;
 import com.chzzk.cushion.member.dto.ApiMember;
-import com.chzzk.cushion.style.domain.ChangeStyleRequestDataGenerator;
 import com.chzzk.cushion.style.domain.ClovaStudioApiExecutor;
+import com.chzzk.cushion.style.domain.RetryChangeStyleRequestDataGenerator;
 import com.chzzk.cushion.style.dto.RetryChangeStyleRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,12 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 import static com.chzzk.cushion.chatroom.domain.SenderType.BOT;
 import static com.chzzk.cushion.chatroom.domain.SenderType.USER;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RetryChangeStyleService {
 
     private final MemberRepository memberRepository;
-    private final ChangeStyleRequestDataGenerator changeStyleRequestDataGenerator;
+    private final RetryChangeStyleRequestDataGenerator retryChangeStyleRequestDataGenerator;
     private final ClovaStudioApiExecutor clovaStudioApiExecutor;
 
     @Transactional
@@ -29,12 +31,17 @@ public class RetryChangeStyleService {
         Member member = apiMember.toMember(memberRepository);
         ChatRoom chatRoom = member.findChatRoomById(request.getRoomId());
         Message latestUserMessage = chatRoom.getLatestMessage(USER);
+        Message latestBotMessage = chatRoom.getLatestMessage(BOT);
 
-        JSONObject requestData = changeStyleRequestDataGenerator
-                .generateWithUserMessage(member, latestUserMessage.getContent(), chatRoom);
+        JSONObject requestData = retryChangeStyleRequestDataGenerator
+                .generateWithUserMessage(
+                        member,
+                        latestUserMessage.getContent(),
+                        latestBotMessage.getContent(),
+                        chatRoom);
+        log.info("requestData = {}", requestData.toJSONString());
         String resultMessage = clovaStudioApiExecutor.changeStyleDefault(requestData);
 
-        Message latestBotMessage = chatRoom.getLatestMessage(BOT);
         latestBotMessage.updateContent(resultMessage);
 
         return resultMessage;
@@ -45,12 +52,17 @@ public class RetryChangeStyleService {
         Member member = apiMember.toMember(memberRepository);
         ChatRoom chatRoom = member.findChatRoomById(request.getRoomId());
         Message latestUserMessage = chatRoom.getLatestMessage(USER);
+        Message latestBotMessage = chatRoom.getLatestMessage(BOT);
 
-        JSONObject requestData = changeStyleRequestDataGenerator
-                .generateWithUserMessageAndPersonality(member, latestUserMessage.getContent(), chatRoom);
+        JSONObject requestData = retryChangeStyleRequestDataGenerator
+                .generateWithUserMessageAndPersonality(
+                        member,
+                        latestUserMessage.getContent(),
+                        latestBotMessage.getContent(),
+                        chatRoom);
+        log.info("requestData = {}", requestData.toJSONString());
         String resultMessage = clovaStudioApiExecutor.changeStyleDefault(requestData);
 
-        Message latestBotMessage = chatRoom.getLatestMessage(BOT);
         latestBotMessage.updateContent(resultMessage);
 
         return resultMessage;
